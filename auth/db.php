f7f56c5e243978285e9ba4addcf7c13e938d7be7
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-13 12:14:25 +0800

    Updated database

--- auth/db.php
+++ auth/db.php
@@ -1,22 +1,9 @@
 
-
-<?php
-/*
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=salespulsedb", "root", "");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-*/
-?>
-
-
 <?php
 try {
     $conn = new PDO("mysql:host=127.0.0.1;port=3306;dbname=u390688067_salespulse_db", "u390688067_un_salespulse", "Pass-salespulse123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection successful!";
+    // echo "Connection successful!";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
