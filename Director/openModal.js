d2257749cb5b68b33f5aa14e9fc7f6101ad96f7a
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-13 10:39:22 +0800

    z

--- Director/openModal.js
+++ Director/openModal.js
@@ -36,7 +36,7 @@
                     document.getElementById('end-date-placeholder').value   = data.stages.stage_one.end_date   || 'No Data';
                     document.getElementById('status-placeholder').value     = data.stages.stage_one.status     || 'No Data';
 
-                    document.getElementById('project-unique-id').value = projectId || 'No Data';
+                    document.getElementById('project-unique-id').value = data.projectId || 'No Data';
                     document.getElementById('client-name').textContent = data.company_name || 'No Data';
 
                     // Populate all stage data
