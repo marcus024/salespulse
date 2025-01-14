321e324851c60750b929eece479eabe9cbf6bb9d
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-14 13:16:23 +0800

    x

--- Director/director.php
+++ Director/director.php
@@ -339,13 +339,22 @@ include("../auth/db.php");
                 onclick="toggleSidebar()" 
                 style="cursor: pointer; font-size: 20px; color: white; margin: 10px;"
             ></i> -->
-            <img 
-                id="sidebarToggleIcon" 
-                src="../images/logo_white.png" 
-                alt="Toggle Sidebar" 
-                onclick="toggleSidebar()" 
-                style="cursor: pointer; width: 24px; height: 24px; margin: 10px;"
-            />
+           <div 
+                class="d-flex align-items-center mx-1" 
+                id="salespulse" 
+                style="font-weight: bold; font-family: 'Poppins';"
+                >
+                <!-- The image outside the text element -->
+                <img
+                    id="sidebarToggleIcon"
+                    src="../images/logo_white.png"
+                    alt="Toggle Sidebar"
+                    onclick="toggleSidebar()"
+                    style="cursor: pointer; width: 24px; height: 24px; margin-right: 10px;"
+                />
+                <!-- The text -->
+                <span>SALES PULSE</span>
+            </div>
             <!-- <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div style="color:white; font-weight:bold; font-family:'Poppins'; font-size:20px">
                     SALES PULSE
