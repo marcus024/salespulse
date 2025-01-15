e885d38484c0da62266f0638e0b3d8c9a821d789
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-15 10:43:34 +0800

    Z

--- admin/projects/view/view_project.php
+++ admin/projects/view/view_project.php
@@ -10,7 +10,7 @@ include_once('../model/view_project.php');
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Sales Pulse</title>
-    <link rel="icon" href="../images/logo_blue.ico" type="image/x-icon">
+    <link rel="icon" href="../../../images/logo_blue.ico" type="image/x-icon">
     <!-- Custom fonts for this template -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
