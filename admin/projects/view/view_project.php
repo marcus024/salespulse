3de1ef4a72847f5bf050311858508f343a2836d3
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-15 12:38:48 +0800

    z

--- admin/projects/view/view_project.php
+++ admin/projects/view/view_project.php
@@ -597,7 +597,7 @@ include_once('../model/view_project.php');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
-    <script src="../../current_year.js"></script>
+    <script src="../../../Director/current_year.js"></script>
     
     
     <script>
