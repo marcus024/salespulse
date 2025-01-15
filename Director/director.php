b65c6811963f7c5a675857e0c8857dd3850f39b9
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-15 09:47:17 +0800

    x

--- Director/director.php
+++ Director/director.php
@@ -10,7 +10,7 @@ include("../auth/db.php");
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>SALES PULSE</title>
+    <title>Sales Pulse</title>
     <link rel="icon" href="../images/logo_blue.ico" type="image/x-icon">
     <!-- Custom fonts for this template-->
 
