b2fe814ca7366cc8baba6d9d67242a3f097d9f4c
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-15 09:49:49 +0800

    Added title icon

--- Director/dirviewproject.php
+++ Director/dirviewproject.php
@@ -9,7 +9,8 @@ include_once('dirback/dirviewback.php');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>SALES PULSE</title>
+    <title>Sales Pulse</title>
+    <link rel="icon" href="../images/logo_blue.ico" type="image/x-icon">
     <!-- Custom fonts for this template -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
