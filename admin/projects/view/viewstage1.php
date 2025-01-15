b806ed38e73110fe1362970a18cb73897a121bf9
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-15 12:58:46 +0800

    z

--- admin/projects/view/viewstage1.php
+++ admin/projects/view/viewstage1.php
@@ -690,7 +690,7 @@ include_once('../model/view_project.php');
     <script src="../../../vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="../../../vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-    <script src="../js/demo/datatables-demo.js"></script>
+    <script src="../../../js/demo/datatables-demo.js"></script>
 
     <script src="../../../Director/toogleNav.js"></script>
     
