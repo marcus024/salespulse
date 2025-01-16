185ca6eb078101c59a28368e45d927c6ae930b0c
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-16 11:07:31 +0800

    z

--- Director/openModal.js
+++ Director/openModal.js
@@ -62,7 +62,7 @@
                     requirements.forEach((requirement) => {
                         const requirementRow = document.createElement('div');
                         requirementRow.className = 'row align-items-center requirement-field';
-                        requirementRow.style.margin = '5px 0 5px 0';
+                        requirementRow.style.margin = '5px 0 0 0';
 
                         requirementRow.innerHTML = `
                             <div class="col-10 d-flex align-items-center">
