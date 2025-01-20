27ce0212ed5c03eca68e87ec28028000d830e31c
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-20 10:23:16 +0800

    Added icon in auth pages

--- auth/register.php
+++ auth/register.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sales Pulse Login</title>
+  <title>Sales Pulse</title>
+  <link rel="icon" href="../images/logo_blue.ico" type="image/x-icon">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
