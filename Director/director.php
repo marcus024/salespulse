13962566ad70d8df84ac6f00a64ef718052dac4f
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-22 17:31:08 +0800

    x

--- Director/director.php
+++ Director/director.php
@@ -1360,7 +1360,7 @@ include("../auth/db.php");
                                                         $schedules = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                                                         if (empty($schedules)) {
-                                                            echo '<div class="p-2 rounded border" style="font-size: 10px; background-color: #2a2925; color: white; border-color:grey;">
+                                                            echo '<div class="p-2 " style="font-size: 10px; background-color: #2a2925; color: white; border:none;">
                                                                     There is no schedule today.
                                                                 </div>';
                                                         } else{
