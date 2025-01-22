a0302e66765c9f50dab99884d790ca32adcb959a
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-22 15:19:48 +0800

    x

--- Director/director.php
+++ Director/director.php
@@ -281,6 +281,7 @@ include("../auth/db.php");
             display: none;
         }
     </style>
+    <!-- navbarstyle -->
     <style>
         .nav-item{
             color: white; 
@@ -332,7 +333,7 @@ include("../auth/db.php");
     <div id="wrapper" >
         <!-- Sidebar -->
         <!-- Sidebar -->
-        <ul class="navbar-nav floating-sidebar" id="accordionSidebar" style="background-color:#000000; width: 200px; transition: all 0.3s; padding-left: 20px;">
+        <ul class="navbar-nav floating-sidebar" id="accordionSidebar" style="background-color:#d1d1d1; width: 200px; transition: all 0.3s; padding-left: 20px;">
             <!-- Sidebar - Brand -->
             <!-- <i 
                 id="sidebarToggleIcon" 
