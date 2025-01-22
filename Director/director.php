487a2e64c4f4c0b3e826fcd36ef0b6b34c51c675
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-22 15:52:59 +0800

    x

--- Director/director.php
+++ Director/director.php
@@ -380,25 +380,25 @@ include("../auth/db.php");
             <hr class="sidebar-divider my-2">
             <!-- Nav Items -->
             <li class="nav-item active" >
-                <a class="nav-link selected" href="" style="border-radius:10px; padding-left:10px;">
+                <a class="nav-link selected" href="" style="border-radius:10px; ">
                     <i class="fas fa-fw fa-home"></i>
                     <span style="font-size:13px; font-family:'Poppins'">Home</span>
                 </a>
             </li>
             <li class="nav-item" >
-                <a class="nav-link" href="calendar.php" style="border-radius:10px; padding-left:10px;">
+                <a class="nav-link" href="calendar.php" style="border-radius:10px; ">
                     <i class="fas fa-fw fa-calendar-alt"></i>
                     <span style="font-size:13px; font-family:'Poppins'">Calendar</span>
                 </a>
             </li>
             <li class="nav-item" >
-                <a class="nav-link" href="contacts.php" style="border-radius:10px; padding-left:10px;">
+                <a class="nav-link" href="contacts.php" style="border-radius:10px; ">
                     <i class="fas fa-fw fa-address-book"></i>
                     <span style="font-size:13px; font-family:'Poppins'">Contacts</span>
                 </a>
             </li>
             <li class="nav-item" >
-                <a class="nav-link" href="team.php" style="border-radius:10px; padding-left:10px;">
+                <a class="nav-link" href="team.php" style="border-radius:10px; ">
                     <i class="fas fa-fw fa-users"></i>
                     <span style="font-size:13px; font-family:'Poppins'">Team Members</span>
                 </a>
