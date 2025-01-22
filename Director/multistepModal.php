56d4edeedd915eb46e2d7323fc9a2c442fa72409
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-22 11:13:56 +0800

    x

--- Director/multistepModal.php
+++ Director/multistepModal.php
@@ -146,7 +146,7 @@
                                             <div id="requirementsContainer">
                                                 <div class="requirement-block" data-index="1">
                                                     <p class="text-center text-white mb-1" style="font-style:'Poppins'; font-weight:bold;">
-                                                    Requirement ${requirementCount}
+                                                    Requirement 
                                                     </p>
                                                     <input type="hidden" name="requirement_id_1[]" value="st1rq1">
                                                     <div class="row mb-2">
