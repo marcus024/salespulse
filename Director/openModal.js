0e654847de762ced136f7900576736f379dc56f2
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-23 14:44:06 +0800

    x

--- Director/openModal.js
+++ Director/openModal.js
@@ -226,14 +226,14 @@ function createRequirementBlock(blockIndex, reqItem, productList, distributorLis
         <input name="requirement_one[]" type="text" class="form-control" placeholder="e.g. Sample Requirement" value="${requirementText}">
       </div>
       <div class="col-md-3">
-        <select name="product_one[]" class="form-control custom-select productFetch">
-          <option disabled ${!selectedProduct ? 'selected' : ''}>Select</option>
-          ${productList.map(product => `
-            <option value="${escapeHtml(product)}" ${product.trim() === selectedProduct.trim() ? 'selected' : ''}>
-              ${product}
+        <select name="distributor_one[]" class="form-control custom-select distributorFetch">
+          <option disabled ${!selectedDistributor ? 'selected' : ''}>Select</option>
+          ${distributorList.map(distributor => `
+            <option value="${escapeHtml(distributor)}" ${distributor.trim() === selectedDistributor.trim() ? 'selected' : ''}>
+              ${escapeHtml(distributor)}
             </option>
           `).join('')}
-          <option value="add_new_product">+ Add New Product...</option>
+          <option value="add_new">+ Add New Distributor...</option>
         </select>
       </div>
       <div class="col-md-3">
