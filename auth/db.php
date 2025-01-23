a6451c2b4f6ccd5ded693ed05405f479b1d06a33
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-23 10:22:44 +0800

    x

--- auth/db.php
+++ auth/db.php
@@ -1,7 +1,7 @@
 
 <?php
 try {
-    $conn = new PDO("mysql:host=127.0.0.1;port=3306;dbname=u390688067_salespulse_db", "u390688067_un_salespulse", "Pass-salespulse123");
+    $conn = new PDO("mysql:host=127.0.0.1;port=3306;dbname=u390688067_salespulse_dbt", "u390688067_un_salespulset", "Pass-salespulse123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connection successful!";
 } catch (PDOException $e) {
