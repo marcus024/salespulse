16b76896e05a6d34f5c8ca1e455bf841cc41bf32
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-24 10:47:26 +0800

    x

--- Director/modal_req/duplicate_req_one.js
+++ Director/modal_req/duplicate_req_one.js
@@ -9,9 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
   initDistributorChangeHandler();
 
   
-  // Load products and distributors and cache them
-  $.when(loadProducts(), loadDistributors()).done(function () {
-    console.log("Products and distributors loaded.");
+  $.when( loadProducts(), loadDistributors() ).done(function() {
+    // If needed, do something after both are loaded
   });
 
   // "Add" button to clone a new requirement block
