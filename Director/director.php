d840c2dfeeaf61626707e9ddb59e1ed343705612
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-25 09:51:41 +0800

    d

--- Director/director.php
+++ Director/director.php
@@ -21,7 +21,7 @@ include("../auth/db.php");
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <!-- <link href="../css/sb-admin-2.min.css" rel="stylesheet"> -->
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom styles for this page -->
     <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
