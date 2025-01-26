a3378fabd85cec341e6754573ca71c2f061020b7
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-26 11:57:37 +0800

    Added duplicate function to stage four requirement

--- Director/multistepModal.php
+++ Director/multistepModal.php
@@ -1079,6 +1079,7 @@
     <script src="modal_req/duplicate_req1_st2.js"></script>
     <script src="modal_req/duplicate_eng_three.js"></script>
     <script src="modal_req/duplicate_req_three.js"></script>
+    <script src="modal_req/duplicate_req_four.js"></script>
     <script>
         function showNotif_bar(message = "Operation completed successfully!", duration = 3000) {
             const notif_bar_r = document.getElementById('notif_bar_r');
