3a80c554fd76f775c6694e1961262659e64bd0c0
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-27 14:39:15 +0800

    x

--- Director/viewprofile.php
+++ Director/viewprofile.php
@@ -326,9 +326,10 @@ include_once('../auth/db.php');
                                         </div>
                                     </div>
                                     <div class="stage-percentage" style="width: 45%; text-align: right; font-size: 16px; color: #36b9cc;">
-                                        <button onclick="if (history.length > 1) { history.back(); } else { window.location.href = 'director.php'; }"  style="background: none; border: none; color: #555; font-size: 24px; cursor: pointer;">
+                                        <!-- <button onclick="if (history.length > 1) { history.back(); } else { window.location.href = 'director.php'; }"  style="background: none; border: none; color: #555; font-size: 24px; cursor: pointer;">
                                             <i class="fas fa-arrow-left"></i>
-                                        </button>
+                                        </button> -->
+                                        <a href="director.php">Back</a>
                                     </div>
                                 </div>
                                 <div class="container" style="background-color: #1f2024; padding: 10px; border-radius: 20px"> 
