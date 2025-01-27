80181712692e61915285e74fd3c076b2a0764b84
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-27 08:55:13 +0800

    x

--- Director/calendar.php
+++ Director/calendar.php
@@ -439,17 +439,17 @@ include("../auth/db.php");
                                     <div class="modal fade" id="addCalendarModal" tabindex="-1" aria-labelledby="addCalendarModalLabel" aria-hidden="true">
                                         <div class="modal-dialog">
                                             <div class="modal-content">
-                                                <div class="modal-header" style="background:#36b9cc">
-                                                    <h5 class="modal-title" style="font-weight:bold; font-size:12px; font-family:'Poppins'; color:white;" id="addCalendarModalLabel">Add Calendar Link</h5>
+                                                <div class="modal-header" style="background:#f9ce45; border:none;">
+                                                    <h5 class="modal-title" style="font-weight:bold; font-size:12px; font-family:'Poppins'; color:#1f2024;" id="addCalendarModalLabel">Add Calendar Link</h5>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
-                                                <div class="modal-body">
+                                                <div class="modal-body" style=" background:#1f2024;">
                                                     <form id="calendarForm">
                                                         <div class="mb-3">
                                                             <label for="calendarLink" style="font-size:12px; font-weight:bold; font-family:'Poppins';" class="form-label">Calendar Link</label>
                                                             <input type="url" class="form-control" id="calendarLink" required>
                                                         </div>
-                                                        <button type="submit" class="btn" style="background:#36b9cc; color:white; font-family:'Poppins'; font-size:15px;">Save</button>
+                                                        <button type="submit" class="btn" style="background:#f9ce45; color:#1f2024; font-family:'Poppins'; font-size:15px;">Save</button>
                                                     </form>
                                                 </div>
                                             </div>
