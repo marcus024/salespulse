a6968cada90149a82090b99fc878b531afbc585b
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-28 15:00:37 +0800

    s

--- Director/viewstage1.php
+++ Director/viewstage1.php
@@ -389,7 +389,7 @@ include_once('dirback/dirviewback.php');
                                                 <div class="animated-bg" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: radial-gradient(circle, rgba(255, 255, 255, 0.2) 0%, transparent 20%); animation: pulse 4s infinite; z-index: 1; pointer-events: none;"></div>
                                                 <!-- Right Section: Stage Percentage -->
                                                 <div class="stage-percentage" style="width: 100%; text-align: left; font-size: 40px; color: #1f2024; z-index: 2; font-weight:bold;">
-                                                    <?php echo htmlspecialchars($project['company_name']); ?>
+                                                    
                                                 </div>
                                                 <!-- Left Section: Stage Title with Vertical Alignment -->
                                                 <div class="stage-title" style="width: 30%; display: flex; flex-direction: column; align-items: flex-end; z-index: 2;">
