7cf5b06506726fb8fab444216f0139928557b8de
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-28 10:38:12 +0800

    x

--- Director/multistepModal.php
+++ Director/multistepModal.php
@@ -60,6 +60,12 @@
                     font-family: 'Poppins';
                     background-color:#1f2024;
                 }
+                .form-control:readonly{
+                    font-size: 12px;
+                    color:white;
+                    font-family: 'Poppins';
+                    background-color:#1f2024;
+                }
                 #multiStepModal .modal-dialog {
                     max-width: 1000px; /* Increased width */
                 }
