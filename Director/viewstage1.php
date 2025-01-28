14787ae57ce9ed57de0aad8930d94277848a7105
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-28 14:18:28 +0800

    s

--- Director/viewstage1.php
+++ Director/viewstage1.php
@@ -354,7 +354,7 @@ include_once('dirback/dirviewback.php');
                                             <div class="stage-container" style="display: flex; justify-content: space-between; align-items: center; padding: 0px;">
                                                 
                                                 <div class="stage-percentage" style="width: 45%; text-align: right; font-size: 16px; color:white;">
-                                                    <p style="color:#555">Progress: <span  style="font-family: 'Poppins'; color:white;">20%</span></p>
+                                                    
                                                 </div>
                                                 <div class="stage-title" style="width: 30%; text-align: left; margin-bottom: 0; padding-bottom: 0;">
                                                     <p id="projectUniqueId" style="color:white; margin-bottom: 5px; font-family: 'Poppins', sans-serif;">Stage 1 <span hidden style="color:rgba(255, 255, 255, 0.9);" id="project-id-placeholder">[Project ID]</span></p> 
