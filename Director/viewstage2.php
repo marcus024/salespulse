7f906af49c093daa53c1ec0edd4b7975c9952e4a
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-01-29 19:19:50 +0800

    x

--- Director/viewstage2.php
+++ Director/viewstage2.php
@@ -393,7 +393,7 @@ include_once('dirback/dirviewback.php');
                                             <img src="../images/project_s.png" alt="Project Icon" style="width: 60px; height: 60px; vertical-align: middle;" />
                                         </div>
                                         <?php include("dirback/pro_stage_details.php"); ?>
-                                        <?php include('dirback/fetch_s1.php'); ?>
+                                        <?php include('dirback/fetch_s2.php'); ?>
                                         <div class="col-md-12 mb-3">
                                             <p style="color:white; margin-top: 0; font-family: 'Poppins'; font-size:30px; font-weight:bold; display: inline;"><?php echo htmlspecialchars($project['company_name']); ?></p>
                                             <p id="projectUniqueId" style="color:white; margin-top: 0; margin-bottom: 5px; font-family: 'Poppins'; font-size: 12px; font-weight: 500;">
