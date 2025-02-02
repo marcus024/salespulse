c1753d38ad17b46ff58897a0b5f4d3b905007c90
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-02 18:35:50 +0800

    x

--- Director/dirback/spcome_fetch_table.php
+++ Director/dirback/spcome_fetch_table.php
@@ -16,12 +16,6 @@ if (!$user_id) {
     exit();
 }
 
-// Check database connection
-if ($conn->connect_error) {
-    http_response_code(500);
-    echo json_encode(['error' => 'Database connection failed: ' . $conn->connect_error]);
-    exit();
-}
 
 $sql = "
     SELECT 
