1d996ba7fd68e8572c4a0b41f4831b06d63c8c84
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-02 20:32:52 +0800

    x

--- Director/director.php
+++ Director/director.php
@@ -1748,7 +1748,7 @@ include("../auth/db.php");
             <div class="modal fade" id="addSchedule" tabindex="-1" aria-labelledby="addProjectModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-lg">
                 <div class="modal-content" style="background: rgba(255, 255, 255, 0.9); backdrop-filter: blur(2px); border-radius: 5px;">
-                <div class="modal-header" style="border:none; background-color:#f9ce45; height: 50px;">
+                <div class="modal-header" style="border:none; font-family:'Poppins'; background-color:#f9ce45; height: 50px;">
                     <font color="white">
                     <h5 class="m" id="addProjectModalLabel">Add New Schedule</h5>
                     </font>
