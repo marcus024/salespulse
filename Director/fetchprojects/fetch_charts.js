ed7f7c30c0f55c9b5567ec6994c48a4c0f5e6af6
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-03 13:15:30 +0800

    c

--- Director/fetchprojects/fetch_charts.js
+++ Director/fetchprojects/fetch_charts.js
@@ -186,7 +186,7 @@ $('#calendar-container').before(`
     <div style=" width: 750px; padding-left:20px; padding-top: 10px;padding-bottom: 0px; padding-right: 20px; display: flex; justify-content: space-between; align-items: center;">
         <!-- Title on the Left -->
         <div>
-            <h4 class="text-white" style="font-family: 'Poppins'; font-weight:bold; font-size: 30px; margin-top: 5px;">Commissions Summary</h4>
+            <h4 class="text-white" style="font-family: 'Poppins'; font-weight:bold; font-size: 30px; margin-top: 5px; margin-right: 20px;">Commissions Summary</h4>
         </div>
         
         <!-- Filter Widget on the Right -->
