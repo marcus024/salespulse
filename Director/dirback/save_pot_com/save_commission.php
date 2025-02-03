f91d54fb6606117ad5b17c56c32b0fc284c072fe
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-03 17:00:28 +0800

    s

--- Director/dirback/save_pot_com/save_commission.php
+++ Director/dirback/save_pot_com/save_commission.php
@@ -10,7 +10,7 @@ $created_at = date('Y-m-d H:i:s');  // Current timestamp
 
 // SQL Insert query
 $sql = "INSERT INTO potentialCom (actual_com, target_com, account_manager,potential_com, created_at)
-        VALUES ('$actual_com', '$target_com', '$account_manager',$potential_com, '$created_at')";
+        VALUES ('$actual_com', '$target_com', '$account_manager','$potential_com', '$created_at')";
 
 // Execute query and respond
 if ($conn->query($sql) === TRUE) {
