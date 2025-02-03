36694e554b64278e479f2894001f494bbe077c82
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-03 10:46:51 +0800

    x

--- Director/fetchprojects/fetch_charts.js
+++ Director/fetchprojects/fetch_charts.js
@@ -158,7 +158,7 @@ function updateCharts(data) {
 
 // HTML for the filter widget (Unchanged)
 $('#calendar-container').before(`
-    <div class="mb-3">
+    <div class="mb-1" style="margin:10px;">
         <label for="filterWidget" class="form-label text-white">Filter by Project</label> 
         <select class="form-select" id="filterWidget">
             <option value="">All</option>
