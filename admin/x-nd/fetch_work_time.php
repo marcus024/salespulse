64b8a1f43570304abbe3477b3484eb8ee06bd239
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-19 17:00:02 +0800

    x

--- admin/x-nd/fetch_work_time.php
+++ admin/x-nd/fetch_work_time.php
@@ -15,7 +15,7 @@ try {
                 w.start_time,
                 w.end_time,
                 w.time AS duration,
-                s.role AS roles
+                s.position AS roles
             FROM workpulse AS w
             JOIN salesauth AS s ON w.user = s.user_id_current";
 
