f848f26b08ad3a9de68d00ccfcc5626bd6bfc25f
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-02-19 13:48:50 +0800

    x

--- Director/spclock.php
+++ Director/spclock.php
@@ -343,7 +343,7 @@ include("../auth/db.php");
 
                         <!-- Right Side: Task Details Widget -->
                         <div class="col-md-6">
-                            <div class="task-details-widget" style="width:800px">
+                            <div class="task-details-widget" style="width:600px">
                                 <p style="font-size:15px; font-weight:bold;">Task Details</p>
                                 <div id="taskDetailsContainer" class="overflow-y-auto max-h-[900px]  p-2 rounded-md shadow-md">
                                     <!-- Task containers will be appended here -->
